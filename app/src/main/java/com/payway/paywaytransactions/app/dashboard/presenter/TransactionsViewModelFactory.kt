63359f0154238b2c784c0a123ca4aa710e0436ca
Ivan Paul <paulivan568@gmail.com>
2023-12-28 17:18:14 +0300

    Add Pie Chart for categories

--- app/src/main/java/com/payway/paywaytransactions/app/dashboard/presenter/TransactionsViewModelFactory.kt
+++ app/src/main/java/com/payway/paywaytransactions/app/dashboard/presenter/TransactionsViewModelFactory.kt
@@ -3,17 +3,19 @@ package com.payway.paywaytransactions.app.dashboard.presenter
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.payway.paywaytransactions.domain.dashboard.usecase.GetLineChartUseCase
+import com.payway.paywaytransactions.domain.dashboard.usecase.GetPieChartUseCase
 import com.payway.paywaytransactions.domain.dashboard.usecase.GetTransactionsUseCase
 
 class TransactionsViewModelFactory(
     private val getTransactionsUseCase: GetTransactionsUseCase,
-    private val getLineChartUseCase: GetLineChartUseCase
+    private val getLineChartUseCase: GetLineChartUseCase,
+    private val getPieChartUseCase: GetPieChartUseCase
 ) : ViewModelProvider.Factory {
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(TransactionsViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
-            return TransactionsViewModel(getTransactionsUseCase,getLineChartUseCase) as T
+            return TransactionsViewModel(getTransactionsUseCase,getLineChartUseCase,getPieChartUseCase) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
